e551ce175bcfe6cfc396ddee213c85d31143024f
Lewie Donckers <l.donckers@xs4all.nl>
2023-09-11 10:02:39 +0200

    Further improvements

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from random import choice
 from typing import List, Tuple
-import copy
 
 import numpy as np
 
@@ -10,30 +9,37 @@ from ...snake import Snake
 
 
 def is_on_grid(pos: np.array, grid_size: Tuple[int, int]) -> bool:
-    """
-    Check if a position is still on the grid
-    """
     return 0 <= pos[0] < grid_size[0] and 0 <= pos[1] < grid_size[1]
 
 
 def collides(pos: np.array, snakes: List[Snake]) -> bool:
-    """
-    Check if a position is occupied by any of the snakes
-    """
     for snake in snakes:
         if snake.collides(pos):
             return True
     return False
 
 
-def distance(a: np.array, b: np.array) -> int:
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+def get_distance(source: np.array, target: np.array) -> int:
+    return abs(source[0] - target[0]) + abs(source[1] - target[1])
+
+
+def get_minimum_distance(source, targets):
+    return min([get_distance(source, target) for target in targets])
+
+
+class Node:
+
+    def __init__(self, position, *, parent=None):
+        self.position = position
+        self.parent = parent
+        self.cost_so_far = 0
+        self.estimated_total = 0
+
+    def __eq__(self, other):
+        return (self.position == other.position).all()
 
 
 class LewieBot(Bot):
-    """
-    Moves randomly, but makes sure it doesn't collide with other snakes
-    """
 
     @property
     def name(self):
@@ -43,125 +49,97 @@ class LewieBot(Bot):
     def contributor(self):
         return 'Lewie'
 
-    # TODO sometimes we go waaay to deep
-    # - can limit max depth
-    # - currently we do depth first. based on most promising path so far. probably better to do depth first.
-    def _do_stuff2(self, snake, other_snake, candies, pos, history, path,
-                   results):
-        print(f"{len(history)} : {history}")
-        options = {}
-        for move in MOVE_VALUE_TO_DIRECTION:
-            new_pos = pos + MOVE_VALUE_TO_DIRECTION[move]
-            first_move = (history + [move])[0]
-            score = len(history) + 1
+    def _is_valid_position(self, position, snake, other_snake):
+        return is_on_grid(position, self.grid_size) and not collides(
+            position, [snake, other_snake])
 
-            if not is_on_grid(new_pos, self.grid_size):
-                print("ignore based on grid")
-                continue
+    def _get_valid_moves(self, position, snake, other_snake):
+        return [
+            move for move in MOVE_VALUE_TO_DIRECTION
+            if self._is_valid_position(
+                position + MOVE_VALUE_TO_DIRECTION[move], snake, other_snake)
+        ]
 
-            if collides(new_pos, [snake, other_snake]):
-                print("ignore based on collision")
-                continue
+    def _get_neighbors(self, current_node, snake, other_snake):
+        return [
+            Node(current_node.position + MOVE_VALUE_TO_DIRECTION[move],
+                 parent=current_node) for move in self._get_valid_moves(
+                     current_node.position, snake, other_snake)
+        ]
 
-            if any([(new_pos == p).all() for p in path]):
-                print("ignore based on history")
-                continue
+    def _get_move_to_candy(self, snake, other_snake, candies):
+        # IDEAS to improve
+        # - remove candies that are inside snake
+        # - use heuristic to determine if candy is inside snake based on
+        #   position of candy in snake and number of turns taken
+        # - select better structures for work and done
 
-            on_candy = False
-            for candy in candies:
-                if (new_pos == candy).all():
-                    on_candy = True
-                    if score < results[first_move][0]:
-                        results[first_move] = [score, 1]
-                    elif score == results[first_move][0]:
-                        results[first_move][1] += 1
-                    break
-            if on_candy:
-                continue
+        work = self._get_neighbors(Node(snake[0]), snake, other_snake)
+        done = []
 
-            min_score_to_closest_candy = sorted(
-                [score + distance(new_pos, candy) for candy in candies])[0]
+        while work:
 
-            if min_score_to_closest_candy >= results[first_move][0]:
-                continue
+            current_index = 0
+            current_node = work[current_index]
+            for index, item in enumerate(work):
+                if item.estimated_total < current_node.estimated_total:
+                    current_index = index
+                    current_node = item
 
-            options[move] = min_score_to_closest_candy
+            work.pop(current_index)
+            done.append(current_node)
 
-        for move, _ in sorted(options.items(), key=lambda x: x[1]):
-            new_snake = copy.deepcopy(snake)
-            new_snake.move(MOVE_VALUE_TO_DIRECTION[move])
-            new_pos = pos + MOVE_VALUE_TO_DIRECTION[move]
-            self._do_stuff2(new_snake, other_snake, candies, new_pos,
-                            history + [move], path + [new_pos], results)
+            if any([(current_node.position == candy).all()
+                    for candy in candies]):
+                first_pos = None
+                while current_node:
+                    if current_node.parent:
+                        first_pos = current_node.position
+                    current_node = current_node.parent
+                for move in MOVE_VALUE_TO_DIRECTION:
+                    if ((first_pos -
+                         snake[0]) == MOVE_VALUE_TO_DIRECTION[move]).all():
+                        return move
+                # TODO handle this
+                raise Exception("TODO should not happen")
 
-    def _do_stuff(self, snake, other_snake, candies):
-        print("------------------------")
-        results = {
-            m: [self.grid_size[0] * self.grid_size[1], 0]
-            for m in MOVE_VALUE_TO_DIRECTION
-        }
+            for neighbor in self._get_neighbors(current_node, snake,
+                                                other_snake):
+                if neighbor in done:
+                    continue
 
-        self._do_stuff2(snake, other_snake, candies, snake[0], [], [], results)
+                neighbor.cost_so_far = current_node.cost_so_far + 1
+                neighbor.estimated_total = neighbor.cost_so_far + get_minimum_distance(
+                    neighbor.position, candies)
 
-        return results
+                try:
+                    index = work.index(neighbor)
+                    node = work[index]
+                    if neighbor.cost_so_far < node.cost_so_far:
+                        node = neighbor  # TODO does this work?
+                except ValueError:
+                    work.append(neighbor)
+
+    def _will_suicide_win(self, snake, other_snake):
+        return len(snake) > (len(other_snake) * 2)
+
+    def _get_suicide_move(self, snake):
+        return [
+            move for move in MOVE_VALUE_TO_DIRECTION
+            if ((snake[0] + MOVE_VALUE_TO_DIRECTION[move]) == snake[1]).all()
+        ][0]
+
+    def _get_backup_move(self, snake, other_snake):
+        return next(iter(self._get_valid_moves(snake[0], snake, other_snake)),
+                    Move.UP)
 
     def determine_next_move(self, snake: Snake, other_snakes: List[Snake],
                             candies: List[np.array]) -> Move:
-        # suicide if that would win the game
-        if len(snake) > (len(other_snakes[0]) * 2):
-            return [
-                move for move in MOVE_VALUE_TO_DIRECTION
-                if ((snake[0] +
-                     MOVE_VALUE_TO_DIRECTION[move]) == snake[1]).all()
-            ][0]
+        other_snake = other_snakes[0]
 
-        result = self._do_stuff(snake, other_snakes[0], candies)
+        if self._will_suicide_win(snake, other_snake):
+            return self._get_suicide_move(snake)
 
-        return sorted(result.items(),
-                      key=lambda x: x[1][0] * 10 - x[1][1])[0][0]
-
-    def _determine_possible_moves(self, snake, other_snake) -> List[Move]:
-        """
-        Return a list with all moves that we want to do. Later we'll choose one from this list randomly. This method
-        will be used during unit-testing
-        """
-        # highest priority, a move that is on the grid
-        on_grid = [
-            move for move in MOVE_VALUE_TO_DIRECTION
-            if is_on_grid(snake[0] +
-                          MOVE_VALUE_TO_DIRECTION[move], self.grid_size)
-        ]
-        if not on_grid:
-            return list(Move)
-
-        # then avoid collisions with other snakes
-        collision_free = [
-            move for move in on_grid if not collides(
-                snake[0] + MOVE_VALUE_TO_DIRECTION[move], [snake, other_snake])
-        ]
-
-        # then avoid a dead end
-        no_dead_ends = [
-            move for move in collision_free if any([
-                is_on_grid(
-                    snake[0] + MOVE_VALUE_TO_DIRECTION[move] +
-                    MOVE_VALUE_TO_DIRECTION[move2], self.grid_size)
-                and not collides(
-                    snake[0] + MOVE_VALUE_TO_DIRECTION[move] +
-                    MOVE_VALUE_TO_DIRECTION[move2], [snake, other_snake])
-                for move2 in MOVE_VALUE_TO_DIRECTION
-            ])
-        ]
-
-        if no_dead_ends:
-            return no_dead_ends
-        if collision_free:
-            return collision_free
-        else:
-            return on_grid
-
-    def choose_move(self, moves: List[Move]) -> Move:
-        """
-        Randomly pick a move
-        """
-        return choice(moves)
+        return self._get_move_to_candy(snake, other_snake,
+                                       candies) or self._get_backup_move(
+                                           snake, other_snake)
